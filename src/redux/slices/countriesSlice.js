efcfe2122c3654c3f85c46f5e47735bd58f370fc
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-09-21 22:00:32 +0100

    Thunk demo implementation

--- src/redux/slices/countriesSlice.js
+++ src/redux/slices/countriesSlice.js
@@ -1,18 +1,52 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+//axios config
 const baseURL = 'https://restcountries.com/v3.1';
 axios.create({
   baseURL,
 });
 
+//Async Thunks
+export const getCountries = createAsyncThunk(
+  'countries/getCountries',
+  async () => {
+    try {
+      const res = await axios.get('/all');
+      console.log(res.data);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+);
+
+export const getCountry = createAsyncThunk(
+  'countries/getCountry',
+  async (name) => {
+    try {
+      const res = await axios.get(`/name/${name}?fullText=true`);
+      console.log(res.data);
+      const { data } = res.data; // destructure data off the arr of res
+      return data;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+);
+
+//State
 const initialState = {
   countries: [],
   country: {},
-  isLoading: true,
+  status: 'idle', //  || succeeded || pending || failed
   darkMode: false,
+  error: null,
 };
 
+//Reducer
 export const countriesSlice = createSlice({
   name: 'countries',
   initialState,
@@ -22,41 +56,36 @@ export const countriesSlice = createSlice({
     setMode(state) {
       state.darkMode = !state.darkMode;
     },
-    getCountries(state, action) {
-      state.countries = action.payload;
-      state.isLoading = false;
-    },
-    getCountry(state, action) {
-      state.country = action.payload;
-      state.isLoading = false;
-    },
+  },
+  extraReducers(builder) {
+    builder
+      //getCountries
+      .addCase(getCountries.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getCountries.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.countries = action.payload;
+      })
+      .addCase(getCountries.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload.message;
+      })
+      //getCountry
+      .addCase(getCountry.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getCountry.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.country = action.payload;
+      })
+      .addCase(getCountry.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload.message;
+      });
   },
 });
 
 export const selectCountries = (state) => state.countriesSlice;
-export const { setMode, getCountries, getCountry } = countriesSlice.actions;
+export const { setMode, getCountries1, getCountry1 } = countriesSlice.actions;
 export default countriesSlice.reducer;
-
-//Async Actions
-
-// export const getCountries = createAsyncThunk('', async () => {
-//   try {
-//     const res = await axios.get('/all');
-//     console.log(res.data);
-//     return res.data;
-//   } catch (err) {
-//     console.log(err);
-//     return err;
-//   }
-// });
-
-// export const getCountry = createAsyncThunk('', async () => {
-//   try {
-//     const res = await axios.get('/name/${name}?fullText=true');
-//     console.log(res.data);
-//     return res.data;
-//   } catch (err) {
-//     console.log(err);
-//     return err;
-//   }
-// });

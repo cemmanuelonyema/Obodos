29c94d406eb9bbbfdcff0c771a8818da806f49df
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-09-23 00:57:23 +0100

    Create selectCountry component and style

---
+++ src/pages/details/StyledDetails.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledDetails = styled.section`
+  .details__container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 3rem;
+
+    button {
+      display: flex;
+      gap: 5px;
+      align-items: center;
+      padding: 1rem 2rem;
+      margin-bottom: 3rem;
+      box-shadow: 0 1px 6px 0 rgb(32 33 36 / 28%);
+    }
+  }
+`;
